37ef07177d513dc5c09af01f6c49e21e623d3c42
takujiru <takujiru6242@gmail.com>
2025-01-18 00:24:20 +0900

    マージ処理完成

--- services/migration_tool/functions/define_src_data/polers_test/sample.py
+++ services/migration_tool/functions/define_src_data/polers_test/sample.py
@@ -6,8 +6,37 @@ import polars as pl
 
 def hanlder(e={}):
 
+  # 移行定義情報をロード
+  final_cols, finel_key_cols, default_values, dfs, dfs_keys = read_definition()
+
+  # 元データを初期化
+  dfs = initialize_dfs(dfs, dfs_keys, finel_key_cols)
+  print(f'dfs: {dfs}')
+  
+  # 元データを順にマージ
+  merged_df = merge_dfs(dfs, finel_key_cols, final_cols)
+
+  # デフォルト値の設定
+  result_df = set_default_values(merged_df, default_values)
+  print(f'result df: {result_df}')
+
+def read_definition():
+  xlsx = pl.read_excel('sample.xlsx', has_header=False)
+
+  ret = pl.DataFrame()
+  for col in xlsx:
+    ret_col = pl.Series()
+    for data in col:
+      x = data
+      print(x)
+      # ret_col.
+    # ret.with_columns(ret_col)
+
+  print(xlsx)
+  print(ret)
+
   final_cols = ['HOGE', 'ID_1', 'ID_2', 'other', 'name', 'age', 'birth', 'lucky_color']
-  key_cols = ['ID_1', 'ID_2']
+  finel_key_cols = ['ID_1', 'ID_2']
   default_values = {
     'other': 'No other information.',
     'name': 'No name',
@@ -15,51 +44,84 @@ def hanlder(e={}):
     'birth': '1990/1/1',
   }
   moto1 = pl.DataFrame({
-    'ID_1': [1, 2, 3, 4],
-    'ID_2': [11, 22, 33, 44],
+    'ID1': [1, 2, 3, 4],
+    'ID2': [11, 22, 33, 44],
     'name': ['A', 'B', 'C', 'D'],
     'age': [10, 20, 30, None],
     'birth': ['1986/12/17', None, '2025/1/17', None],
   })
+  moto1_key_cols = ['ID1', 'ID2']
   moto2 = pl.DataFrame({
-    'ID_1': [1, 3, 5, 2, 4],
-    'ID_2': [11, 23, 55, 22, 44],
+    'ID__1': [1, 3, 5, 2, 4],
+    'ID__2': [11, 23, 55, 22, 44],
     'name': ['AAA', 'CCC', 'EEE', None, ''],
     'lucky_color': ['red', 'yellow', 'blue', None, None]
   })
+  moto2_key_cols = ['ID__1', 'ID__2']
 
-  # 両方のキーをマージ
-  key = pl.concat([moto1[key_cols], moto2[key_cols]]).unique().sort(by=key_cols)
-  print(f'key: {key}')
+  return (
+    final_cols,
+    finel_key_cols,
+    default_values,
+    [moto1, moto2],
+    [moto1_key_cols, moto2_key_cols]
+  )
 
-  # 元データを順番にマージ
-  prev_df = key
-  for next_df in [moto1, moto2]:
+def initialize_dfs(dfs, dfs_keys, finel_key_cols):
+  # 全データをstrにキャスト
+  dfs = [cast_to_str(df) for df in dfs]
+  # キーの名前を統一
+  result_dfs = []
+  for idx_df, df in enumerate(dfs):
+    query = []
+    for idx_col, col in enumerate(df.columns):
+      # キーのカラムの場合、最終形のカラム名に訂正
+      if col in dfs_keys[idx_df]:
+        print(f'Its key: {col}')
+        query.append(pl.col(col).alias(finel_key_cols[idx_col]))
+      else:
+        print(f'Its not key: {col}')
+        query.append(pl.col(col))
+    
+    # 列名変更後のdfを返却値に追加
+    print(f'query: {query}')
+    result_dfs.append(df.select(query))
+
+  return result_dfs
+
+def cast_to_str(df):
+  return df.with_columns(
+    [pl.col(col).cast(pl.Utf8).alias(col) for col in df.columns]
+  )
+
+def merge_dfs(dfs, finel_key_cols, final_cols):
+  # 両方のキーをマージし、キーのみのdfを作成
+  key_only_df = pl.concat([df[finel_key_cols] for df in dfs]).unique().sort(by=finel_key_cols)
+  print(f'key: {key_only_df}')
+
+  # 元データのdfを順番にマージ
+  prev_df = key_only_df
+  for next_df in dfs:
     # マージ用のクエリを生成
     merge_query = create_merge_query(
       final_cols,
       prev_df.columns,
       next_df.columns,
-      key_cols,
-      default_values,
+      finel_key_cols,
     )
     print(f'merge query: {merge_query}')
     # マージの実行
-    prev_df = prev_df.join(next_df, on=key_cols, how='left').select(merge_query)
+    prev_df = prev_df.join(next_df, on=finel_key_cols, how='left').select(merge_query)
     print(f'merged df: {prev_df}')
 
-  # 全データを文字列型に変換
-  result_df = cast_to_str(prev_df)
-  # デフォルト値の設定
-  result_df = set_default_values(result_df, default_values)
-  print(f'result df: {result_df}')
+  return prev_df
 
-def create_merge_query(final_cols, prev_cols, next_cols, key_cols, default_values):
+def create_merge_query(final_cols, prev_cols, next_cols, finel_key_cols):
   query = []
   # 最終結果用のカラムごとにループ
   for col in final_cols:
     # キーの場合は変更なし
-    if col in key_cols:
+    if col in finel_key_cols:
       query.append(pl.col(col))
       continue
     
@@ -83,19 +145,11 @@ def create_merge_query(final_cols, prev_cols, next_cols, key_cols, default_value
       # 先データのみ存在する場合は先データを利用
       if col in next_cols:
         query.append(pl.col(col))
-      # 元データ、先データのどちらにも存在しない場合はデフォルト値を設定
+      # 元データ、先データのどちらにも存在しない場合はnull値を設定
       else:
-        default_value = ''
-        if col in default_values:
-          default_value = default_values.get(col)
-        query.append(pl.lit(default_value).alias(col))
+        query.append(pl.lit(None).alias(col))
   
   return query
-
-def cast_to_str(df):
-  return df.with_columns(
-    [pl.col(col).cast(pl.Utf8).alias(col) for col in df.columns]
-  )
 
 def set_default_values(df, default_values):
   query = []
@@ -116,22 +170,6 @@ def set_default_values(df, default_values):
       query.append(pl.col(col))
 
   return df.select(query)
-
-
-def read_excel():
-  xlsx = pl.read_excel('sample.xlsx', has_header=False)
-
-  ret = pl.DataFrame()
-  for col in xlsx:
-    ret_col = pl.Series()
-    for data in col:
-      x = data
-      print(x)
-      # ret_col.
-    # ret.with_columns(ret_col)
-
-  print(xlsx)
-  print(ret)
 
 if __name__ == '__main__':
     hanlder()
