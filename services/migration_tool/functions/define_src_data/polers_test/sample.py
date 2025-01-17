90ed6fae3a83dc61021fa02f54886d3777eeea3e
takujiru <takujiru6242@gmail.com>
2025-01-17 23:45:37 +0900

    マージ処理おおよそ完成

--- services/migration_tool/functions/define_src_data/polers_test/sample.py
+++ services/migration_tool/functions/define_src_data/polers_test/sample.py
@@ -5,33 +5,14 @@ import polars as pl
 # df.get_column("Integer").is_in([2, 4, 6])
 
 def hanlder(e={}):
-  # test_concat()
-  test_concat2()
-  # test3()
 
-def test3():
-  xlsx = pl.read_excel('sample.xlsx', has_header=False)
-
-  ret = pl.DataFrame()
-  for col in xlsx:
-    ret_col = pl.Series()
-    for data in col:
-      x = data
-      print(x)
-      # ret_col.
-    # ret.with_columns(ret_col)
-
-  print(xlsx)
-  print(ret)
-
-def test_concat2():
-  final_cols = ['ID_1', 'ID_2', 'other', 'name', 'age', 'birth', 'lucky_color']
+  final_cols = ['HOGE', 'ID_1', 'ID_2', 'other', 'name', 'age', 'birth', 'lucky_color']
   key_cols = ['ID_1', 'ID_2']
   default_values = {
-    'other': 'defult_other',
-    'name': '',
-    'age': 0,
-    'birth': '',
+    'other': 'No other information.',
+    'name': 'No name',
+    'age': 99,
+    'birth': '1990/1/1',
   }
   moto1 = pl.DataFrame({
     'ID_1': [1, 2, 3, 4],
@@ -51,7 +32,7 @@ def test_concat2():
   key = pl.concat([moto1[key_cols], moto2[key_cols]]).unique().sort(by=key_cols)
   print(f'key: {key}')
 
-  # データをマージ
+  # 元データを順番にマージ
   prev_df = key
   for next_df in [moto1, moto2]:
     # マージ用のクエリを生成
@@ -63,9 +44,15 @@ def test_concat2():
       default_values,
     )
     print(f'merge query: {merge_query}')
-    # マージ
+    # マージの実行
     prev_df = prev_df.join(next_df, on=key_cols, how='left').select(merge_query)
     print(f'merged df: {prev_df}')
+
+  # 全データを文字列型に変換
+  result_df = cast_to_str(prev_df)
+  # デフォルト値の設定
+  result_df = set_default_values(result_df, default_values)
+  print(f'result df: {result_df}')
 
 def create_merge_query(final_cols, prev_cols, next_cols, key_cols, default_values):
   query = []
@@ -105,36 +92,46 @@ def create_merge_query(final_cols, prev_cols, next_cols, key_cols, default_value
   
   return query
 
-def test_concat():
-  # 最終データ
-  saki = [
-      pl.col('ID'),
-      pl.col('name1').fill_null(''),
-      pl.col('name2').fill_null(''),
-      # pl.col('name3').fill_null(''),
-      # pl.col('name4').fill_null('999'),
-  ]
+def cast_to_str(df):
+  return df.with_columns(
+    [pl.col(col).cast(pl.Utf8).alias(col) for col in df.columns]
+  )
 
-  # name2, 3, 4は無い元データ
-  moto1 = pl.DataFrame({
-   'ID': [1, 2, 3],
-   'name1': ['n1', 'n2', 'n3']
-  })
+def set_default_values(df, default_values):
+  query = []
+  for col in df.columns:
+    # 初期値設定がある場合は、Null, 空文字を初期値で上書き
+    if col in default_values:
+      default_value = default_values.get(col)
+      query.append(
+        pl.when(
+          (pl.col(col).is_null())
+          | (pl.col(col) == '')
+        ).then(pl.lit(default_value))
+        .otherwise(pl.col(col))
+        .alias(col)
+      )
+    else:
+      # 初期値設定が無い場合は、何もしない
+      query.append(pl.col(col))
 
-  # name1が上書き、name3,4は無い元データ
-  moto2 = pl.DataFrame({
-   'ID': [1, 2, 3],
-   'name1': ['l1', 'm2', 'n3'],
-   'name2': ['n1', 'n2', 'n3']
-  })
+  return df.select(query)
 
-  result = moto1
-  for df in [moto2]:
-    result = result.join(df, on='ID', how='full') \
-      .select(saki)
-  
-  print(result)
 
+def read_excel():
+  xlsx = pl.read_excel('sample.xlsx', has_header=False)
+
+  ret = pl.DataFrame()
+  for col in xlsx:
+    ret_col = pl.Series()
+    for data in col:
+      x = data
+      print(x)
+      # ret_col.
+    # ret.with_columns(ret_col)
+
+  print(xlsx)
+  print(ret)
 
 if __name__ == '__main__':
     hanlder()
